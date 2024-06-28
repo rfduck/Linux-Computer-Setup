11e60ee7075d48481d130381c4ffa76ae031b19f
Russell Fegles <rfegles1@gmail.com>
2024-06-28 14:17:04 -0700

    Adding tumbleweed install script to repo

---
+++ installscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+zypper ref &&
+
+sudo zypper in bat bat-extras blueman btop cups curl distrobox fastfetch fd flatpak fontawesome-fonts fuzzel fzf gammastep grim imv k3b kitty lxappearance mtp-tools neovim opi perl-File-Rename playerctl podman qalculate qt5ct rclone ripgrep rsync slurp stow sway swaybg swayidle swaylock vifm vifm-colors vim waybar wev wl-clipboard yazi zoxide
